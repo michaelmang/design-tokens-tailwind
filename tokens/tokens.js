5415eaf02a20ea618ee6e7a44992f6f83b252148
Mike Mangialardi <mike.mangialardi@rackspace.com>
2021-02-15 11:39:35 -0500

    Add tokens

---
+++ tokens/tokens.js
@@ -0,0 +1,162 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 15 Feb 2021 16:35:23 GMT
+ */
+
+module.exports = {
+  "color": {
+    "primary": {
+      "value": "#3a4d7e",
+      "type": "color",
+      "original": {
+        "value": "#3A4D7E",
+        "type": "color"
+      },
+      "name": "colorPrimary",
+      "attributes": {
+        "category": "color",
+        "type": "primary"
+      },
+      "path": [
+        "color",
+        "primary"
+      ]
+    },
+    "primary_light": {
+      "value": "#b9c4df",
+      "type": "color",
+      "original": {
+        "value": "#B9C4DF",
+        "type": "color"
+      },
+      "name": "colorPrimaryLight",
+      "attributes": {
+        "category": "color",
+        "type": "primary_light"
+      },
+      "path": [
+        "color",
+        "primary_light"
+      ]
+    },
+    "black": {
+      "value": "#29322e",
+      "type": "color",
+      "original": {
+        "value": "#29322E",
+        "type": "color"
+      },
+      "name": "colorBlack",
+      "attributes": {
+        "category": "color",
+        "type": "black"
+      },
+      "path": [
+        "color",
+        "black"
+      ]
+    },
+    "black_light": {
+      "value": "#404f48",
+      "type": "color",
+      "original": {
+        "value": "#404F48",
+        "type": "color"
+      },
+      "name": "colorBlackLight",
+      "attributes": {
+        "category": "color",
+        "type": "black_light"
+      },
+      "path": [
+        "color",
+        "black_light"
+      ]
+    },
+    "white": {
+      "value": "#ffffff",
+      "type": "color",
+      "original": {
+        "value": "#FFFFFF",
+        "type": "color"
+      },
+      "name": "colorWhite",
+      "attributes": {
+        "category": "color",
+        "type": "white"
+      },
+      "path": [
+        "color",
+        "white"
+      ]
+    },
+    "white_dark": {
+      "value": "#f1f5f8",
+      "type": "color",
+      "original": {
+        "value": "#F1F5F8",
+        "type": "color"
+      },
+      "name": "colorWhiteDark",
+      "attributes": {
+        "category": "color",
+        "type": "white_dark"
+      },
+      "path": [
+        "color",
+        "white_dark"
+      ]
+    },
+    "error": {
+      "value": "#eb6957",
+      "type": "color",
+      "original": {
+        "value": "#EB6957",
+        "type": "color"
+      },
+      "name": "colorError",
+      "attributes": {
+        "category": "color",
+        "type": "error"
+      },
+      "path": [
+        "color",
+        "error"
+      ]
+    },
+    "warning": {
+      "value": "#f2c94c",
+      "type": "color",
+      "original": {
+        "value": "#F2C94C",
+        "type": "color"
+      },
+      "name": "colorWarning",
+      "attributes": {
+        "category": "color",
+        "type": "warning"
+      },
+      "path": [
+        "color",
+        "warning"
+      ]
+    },
+    "info": {
+      "value": "#aac2ff",
+      "type": "color",
+      "original": {
+        "value": "#AAC2FF",
+        "type": "color"
+      },
+      "name": "colorInfo",
+      "attributes": {
+        "category": "color",
+        "type": "info"
+      },
+      "path": [
+        "color",
+        "info"
+      ]
+    }
+  }
+};
